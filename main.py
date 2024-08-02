14056e0895051a09ba2f445e5e58f1bed8faec54
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-08-02 17:33:25 -0300

    Add lista_ordenada em main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from sorteio import sorteio_unitario
+from sorteio import sorteio_unitario, sorteio_ordenado
 import streamlit as st
 
 
 with st.form("my_form"):
    st.write("Formulário para sorteio de organizador de HH!!")
    nomes = st.text_input("Quais os nomes você gostaria de sortear?")
+   ordenada = st.toggle('Lista ordenada?')
    submit = st.form_submit_button('Sortear')
    if submit:
-    sorteado = sorteio_unitario(nomes)
-    st.write(f'O sorteado foi {sorteado}.')
+    if ordenada:
+        sorteados = sorteio_ordenado(nomes)
+        count = 1
+        for sorteado in sorteados:
+            st.write(f'O {count}° sorteado foi {sorteado}.')
+            count += 1
+    else:
+        sorteado = sorteio_unitario(nomes)
+        st.write(f'O sorteado foi {sorteado}.')
