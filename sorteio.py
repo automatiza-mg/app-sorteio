218059e45735d4473d74a671b8a32b6e2ee0449f
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-08-02 17:33:42 -0300

    Add lista ordaneda modulo sorteio

--- sorteio.py
+++ sorteio.py
@@ -5,3 +5,9 @@ def sorteio_unitario(nomes):
     nomes_list = nomes.split(',')
     numero_sorteado = random.randrange(0, len(nomes_list))
     return nomes_list[numero_sorteado].strip()
+
+def sorteio_ordenado(nomes):
+    # import ipdb; ipdb.set_trace(context=10)
+    nomes_list = nomes.split(',')
+    random.shuffle(nomes_list)
+    return nomes_list
